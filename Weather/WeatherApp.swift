36351cc5e26f11191b3d879b4edb8bdb49e31bca
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-26 23:51:25 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Malith Kavinda on 2024-10-26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Malith Kavinda on 2024-10-26.
+//
+
+import Testing
+@testable import Weather
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
