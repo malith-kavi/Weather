8d9b41f1068e47f4ca9eaa4268ab1c60fcc0acc7
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-28 01:29:40 +0530

    Create Extensions.swift

---
+++ Weather/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  Weather
+//
+//  Created by Malith Kavinda on 2024-10-28.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format:"%.0f",self)
+    }
+}
