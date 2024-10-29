b69493bc9fd58a9df15339c0f118414f33a644fd
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-30 02:16:04 +0530

    Update WeatherView.swift

--- Weather/Views/WeatherView.swift
+++ Weather/Views/WeatherView.swift
@@ -32,7 +32,7 @@ struct WeatherView: View {
                             
                             Text(weather.weather[0].main)
                         }
-                        .frame(width:150, alignment: .leading)
+                        .frame(width:120, alignment: .leading)
                         
                         Spacer()
                         
